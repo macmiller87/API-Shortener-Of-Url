502efaf7ca3e6cab6f5d4bb5d8c3b192278837ae
macmiller87 <macamagolf@gmail.com>
2025-02-23 16:09:17 -0300

    Refact: setup prisma docker and update actions

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8080);
+  await app.listen(process.env.PORT ?? process.env.SERVER_PORT);
 
   console.log(`Server is running at ${await app.getUrl()} 🚀`);
 }
 
-void bootstrap();
+bootstrap();
